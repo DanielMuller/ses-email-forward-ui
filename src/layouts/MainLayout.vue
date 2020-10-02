6a0df32506e0f096b8c8f4d7c166f059e30f018d
Daniel Muller <github@daniel.ctrlaltdel.ch>
2020-10-02 14:20:42 +0100

    Quasar init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  q-layout(view="hHh lpR fFf")
+    q-header.bg-primary.text-white(elevated)
+      q-toolbar
+        q-btn(dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen")
+        q-toolbar-title
+          q-avatar
+            img(src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg")
+          span.q-ml-sm Email Forwards Manager
+
+    q-drawer(show-if-above v-model="leftDrawerOpen" side="left" bordered)
+    q-page-container
+      router-view
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  q-page.flex.flex-center
+    img(
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    )
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
